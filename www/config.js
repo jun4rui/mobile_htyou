cd30ee9e2a8bf2271a51c006832ecbb28c34e0a8
jun4rui <jun4rui@gmail.com>
2015-10-28 10:31:23 +0800

    尝试用动画转屏，但是效果较差已经放弃还原

--- www/config.js
+++ www/config.js
@@ -34,3 +34,32 @@ function getParameterValue(inUrl/*输入Url*/, inName/*参数名*/){
     return '';
 }
 //TEST: getParameterValue('http://www.htyou.com/index.html?q=北京&asdf=123&zxcv=123&a=fasdf','q');
+
+//底部上划载入特效
+function slide(hrf){
+    var options = {
+        "href"           : hrf,
+        "direction"      : "up", // 'left|right|up|down', default 'left' (which is like 'next')
+        "duration"       :  1000, // in milliseconds (ms), default 400
+        "slowdownfactor" :    4, // overlap views (higher number is more) or no overlap (1), default 3
+        "iosdelay"       :  100, // ms to wait for the iOS webview to update before animation kicks in, default 50
+        "androiddelay"   :  50  // same as above but for Android, default 50
+    };
+    window.plugins.nativepagetransitions.slide(
+        options,
+        function (msg) {console.log("success: "+msg)}, // called when the animation has finished
+        function (msg) {alert("error: "+msg)} // called in case you pass in weird values
+    );
+}
+
+//翻转载入动画特效
+function flip(href) {
+    window.plugins.nativepagetransitions.flip({
+        'duration': 1000,
+        'direction': 'right',
+        'iosdelay': 100,
+        'androiddelay': 50,
+        'winphonedelay': 800,
+        'href': href
+    });
+}
