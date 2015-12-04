1133d4b5178f5a5d2e6f09ce9e9556871d9c3da4
jun4rui <jun4rui@gmail.com>
2015-12-04 16:03:29 +0800

    第三次正式发布前的细小改动和框架更新

--- www/test.js
+++ www/test.js
@@ -1,1 +1,16 @@
-$('#qqLoginBtn>a').html()
+server_addr = 'http://www.htyou.com';
+$.getJSON(server_addr+'/common/adpic_queryAdPic.action?adPicType=220089&datatype=json&jsoncallback=?', function (result, status) {
+    for (var i=0; i<result.length; i++){
+        //转换搜索区域的链接地址
+        //转换搜索关键字的链接地址
+        if (result[i].alink.indexOf('tour/type/month/keyword')==0){
+            result[i].alink = 'tour-list.html?q='+result[i].alink.substring(result[i].alink.lastIndexOf('/keyword')+8,result[i].alink.lastIndexOf('.'));
+        }
+        //转换直达链接的链接地址
+        if (result[i].alink.indexOf('tour/tourbrowse/')==0){
+            result[i].alink = 'mobile_htyou/www/tour-detail.html?lineid='+result[i].alink.substring(result[i].alink.lastIndexOf('/')+1,result[i].alink.lastIndexOf('.'));
+        }
+        console.log('link'+i+':'+result[i].alink);
+    }
+    galleryVM.list = result;
+});
