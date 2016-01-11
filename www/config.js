b75ce58bb8e8c94b14994281c32b17f3db7f3163
jun4rui <jun4rui@gmail.com>
2016-01-11 15:51:18 +0800

    设定销售人员信息超过24小时没有更新就摧毁

--- www/config.js
+++ www/config.js
@@ -110,43 +110,87 @@ function doBack(){
 
 //Caojun 20160108 新增:销售人员信息读取函数
 //销售人员根据url中infoid=???参数进行识别,???代表销售人员的ID编号，通过接口
-//    http://www.htyou.com/common/websinfo_queryWebsInfos.action?submit=ajax&infoID=431
+//http://www.htyou.com/common/websinfo_queryWebsInfos.action?submit=ajax&infoID=431
+//http://www.htyou.com/common/websinfo_queryWebsInfos.action?info_url=oiZKXjjPYlSmiN3yndvDLZ7-E-jA
 //获取，返回值为json格式，销售人员返回值中必须是"info_class": 421
+//INFO_DATA格式：姓名,电话,头像url,存储时间
 (function(){
 	var infoID  = getParameterValue(window.location.href,'infoid');
+	var userID  = getParameterValue(window.location.href,'url');
+	var beforeULTime    = 0;
+	var nowULTime       = 0;
+
+	//INFO_DATA
+	$(window).bind('beforeunload',function(){
+		beforeULTime = (new Date).getTime();
+		console.log('BEFORE:'+(new Date).getTime());
+	});
+	$(window).bind('unload',function(){
+		console.log('UNLOAD:'+(new Date).getTime());
+		nowULTime = (new Date).getTime();
+		console.log('VAL',nowULTime-beforeULTime);
+		console.log('END.');
+	});
 
 	//展现销售人员UI的函数
 	function showSellerUI(){
 		var infoData= '';
 		//读取localStorage中的INFO_DATA数据
 		if ((infoData = window.localStorage.getItem('INFO_DATA'))!=null){
-            console.log(infoData);
-			$('#seller').ready(function(){
-				//设置客服参数
-                $('#seller .face').css({'background':'url('+infoData.split(',')[2]+') 50% 0% no-repeat'});
-                $('#seller .content strong').text(infoData.split(',')[0]);
-                $('#seller .content a').attr('href','tel:'+infoData.split(',')[1]);
-				//弹出UI
-				$('#seller').animate({'left':'1rem','bottom:':'1rem'});
-			});
+			//console.log(infoData);
+			//如果保存时间太久超过24小时则摧毁INFO_DATA，否则显示销售员信息
+			if(((new Date).getTime()-infoData.split(',')[3])>86400000){
+				//console.log((new Date).getTime()-parseInt(infoData.split(',')[3]));
+				window.localStorage.removeItem('INFO_DATA');
+			}else{
+				$('#seller').ready(function(){
+					//设置客服参数
+					$('#seller .face').css({'background':'url('+infoData.split(',')[2]+') 50% 0% no-repeat'});
+					$('#seller .content strong').text(infoData.split(',')[0]);
+					$('#seller .content a').attr('href','tel:'+infoData.split(',')[1]);
+					//弹出UI
+					$('#seller').animate({'left':'1rem','bottom:':'1rem'});
+				});
+			}
+
 		}
 	}
 
-	//处理流程
-	if (infoID!=''){
-		$.getJSON('http://www.htyou.com/common/websinfo_queryWebsInfos.action?submit=ajax&jsoncallback=?&infoID='+infoID,function(result){
-			//是否找到销售人员
-			if (result.length!=0){
-				//判断是否是销售人员类型
-				if (result[0].info_class=='421'){
-					console.log(result[0]);
-					//保存到localStorage中的INFO_DATA中
-					window.localStorage.setItem('INFO_DATA',''+result[0].info_name+','+result[0].description+','+'http://www.htyou.com/'+result[0].info_thumbpic);
+	//有infoID或者userID的处理流程，因为是ajax异步处理数据，所以两种情况作单独处理
+	if (infoID != '' || userID != '') {
+		//用infoID的情况
+		if (infoID != '') {
+			$.getJSON('http://www.htyou.com/common/websinfo_queryWebsInfos.action?submit=ajax&jsoncallback=?&infoID=' + infoID, function (result) {
+				//是否找到销售人员
+				if (result.length != 0) {
+					//判断是否是销售人员类型
+					if (result[0].info_class == '421') {
+						console.log(result[0]);
+						//保存到localStorage中的INFO_DATA中
+						window.localStorage.setItem('INFO_DATA', '' + result[0].info_name + ',' + result[0].description + ',' + 'http://www.htyou.com/' + result[0].info_thumbpic+','+(new Date).getTime());
+					}
 				}
-			}
-			//然后在当前页面加入销售人员UI效果
-			showSellerUI();
-		});
+				//然后在当前页面加入销售人员UI效果
+				showSellerUI();
+			});
+		}
+		//用userID的情况
+		if (userID != '') {
+			$.getJSON('http://www.htyou.com/common/websinfo_queryWebsInfos.action?submit=ajax&jsoncallback=?&url=' + userID, function (result) {
+				//是否找到销售人员
+				if (result.length != 0) {
+					//判断是否是销售人员类型
+					if (result[0].info_class == '421') {
+						console.log(result[0]);
+						//保存到localStorage中的INFO_DATA中
+						window.localStorage.setItem('INFO_DATA', '' + result[0].info_name + ',' + result[0].description + ',' + 'http://www.htyou.com/' + result[0].info_thumbpic+','+(new Date).getTime());
+					}
+				}
+				//然后在当前页面加入销售人员UI效果
+				showSellerUI();
+			});
+		}
+
 	}else{
 		//没有则直接展示销售UI(localStorage中有数据才会展示)
 		showSellerUI();
