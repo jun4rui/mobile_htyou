66a06c39bf59ec8f667a5a8e41dc5759c64d104b
jun4rui <jun4rui@gmail.com>
2016-09-22 16:52:06 +0800

    Add: 行程中的图片加入了文字描述，采用input组件如果文字太长可以左右拖动

--- www/test.js
+++ www/test.js
@@ -1,65 +1,10 @@
-(function (window) {
-	function computeComplex(password) {
-		var complex = 0;
-		var length  = password.length;
+(function(){
+	//找到每一张景点图
+	$('.htyou_album').each(function(){
+		//获得这张图的位置
+		//在这张图下面添加一个遮罩层
+		//在这张图下面添加一个文字层
+		//
 
-		var pre     = '';
-		var preType = 0;
-		for (var i = 0; i < length; i++) {
-			var cur     = password.charAt(i);
-			var curType = gettype(password, i);
-
-			if (preType != curType || !isregular(cur, pre, curType)) {
-				complex += curType + getcomplex(curType, preType);
-			}
-
-			pre     = cur;
-			preType = curType;
-		}
-
-		return complex;
-	}
-
-	function gettype(str, i) {
-		if (str.charCodeAt(i) >= 48 && str.charCodeAt(i) <= 57) {
-			return 1;
-		}
-		else if (str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122) {
-			return 2;
-		}
-		else if (str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90) {
-			return 3;
-		}
-
-		return 4;
-	}
-
-	function isregular(cur, pre, type) {
-		var curCode = cur.charCodeAt(0);
-		var preCode = pre.charCodeAt(0);
-
-		if (curCode - preCode == 0) {
-			return true;
-		}
-
-		if (type != 4 && (curCode - preCode == 1 || curCode - preCode == -1)) {
-			return true;
-		}
-
-		return false;
-	}
-
-	function getcomplex(curType, preType) {
-		if (preType == 0 || curType == preType) {
-			return 0;
-		} else if (curType == 4 || preType == 4) {
-			return 2;
-		} else {
-			return 1;
-		}
-	}
-
-	return window.computeComplex = computeComplex;
-})(window)
-
-computeComplex('caojun');
+	});
+})(window);
